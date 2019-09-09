742078792197a9d66cdd3b4e2d4ecb414534e41d
Olivia <ortufte@gmail.com>
2019-09-09 15:55:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,10 +3,11 @@ def starts_with_a_vowel?(word)
     return true
   else
     return false 
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  
 end
 
 def words_five_letters_long(text)

7e74b98d0f05b4a614a067585cc2604565974b5e
Olivia <ortufte@gmail.com>
2019-09-09 16:03:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match (/^[aeiou]/)
-    return true 
-  else
-    return false
-  end
+ word.match? {|word| word.match
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
